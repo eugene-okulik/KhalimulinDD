e83157f03e9e14b039a847a95a81072d8f6b7f41
KhalimulinDD <dima.khalimulin@mail.ru>
2024-08-11 13:14:45 +0300

    Corrected index from space to colon

--- homework/dmitriy_khalimulin/Lesson5/task_2.py
+++ homework/dmitriy_khalimulin/Lesson5/task_2.py
@@ -5,11 +5,15 @@ operation2 = "результат операции: 514"
 result_of_work = "результат работы программы: 9"
 
 # Находим индекс чисел внутри строки
-index1 = operation1.rfind(" ")
-index2 = operation2.rfind(" ")
-index3 = result_of_work.rfind(" ")
+index1 = operation1.index(":")
+index2 = operation2.index(":")
+index3 = result_of_work.index(":")
+
+index1 = int(operation1[index1:].strip(":"))
+index2 = int(operation2[index2:].strip(":"))
+index3 = int(result_of_work[index3:].strip(":"))
 
 # Находим числа с помошью индекса, преобразовываем их в Int и прибавляем к ним 10
-print(int(operation1[index1:]) + 10)
-print(int(operation2[index2:]) + 10)
-print(int(result_of_work[index3:]) + 10)
+print(index1 + 10)
+print(index2 + 10)
+print(index3 + 10)
