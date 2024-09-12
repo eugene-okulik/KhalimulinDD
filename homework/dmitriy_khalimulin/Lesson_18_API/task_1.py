da6d6d27dd2d137100eb501a5ee5cbcc431d1df5
KhalimulinDD <dima.khalimulin@mail.ru>
2024-09-12 13:07:09 +0300

    Optimization of methods

--- homework/dmitriy_khalimulin/Lesson_18_API/task_1.py
+++ homework/dmitriy_khalimulin/Lesson_18_API/task_1.py
@@ -1,8 +1,8 @@
 import requests
 
 
-# Создание обьекта №1
-def create_object_1():
+# Создание обьекта
+def create_object():
     body = {
         "name": "Honor 10",
         "data": {
@@ -21,46 +21,15 @@ def create_object_1():
     print(response.json())
     assert response.status_code == 200, 'Status code is incorrect'
     assert response.json()['name'] == 'Honor 10'
-    object_id_1 = response.json()['id']
-    return object_id_1
-
-
-# Очистка обьекта №1 после создания
-def clear_object_1(object_id_1):
-    delete_response = requests.delete(f'https://api.restful-api.dev/objects/{object_id_1}')
-    print(delete_response.json())
-
-
-clear_object_1(create_object_1())
+    object_id = response.json()['id']
+    return object_id
 
 
 # ----------------------------------------------------------------------------------------------------
 
 
-# Создание обьекта №2
-def create_object_2():
-    body = {
-        "name": "Honor 50",
-        "data": {
-            "year": 3232,
-            "price": 3334.99,
-            "CPU model": "Intel Core i7",
-            "Hard disk size": "1 TB"
-        }
-    }
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(
-        'https://api.restful-api.dev/objects',
-        json=body,
-        headers=headers
-    )
-    print(response.json())
-    object_id_2 = response.json()['id']
-    return object_id_2
-
-
-# Изменение обьекта №2 методом PUT
-def update_object_2(object_id_2):
+# Изменение обьекта методом PUT
+def update_object_put(object_id):
     body = {
         "name": "Honor 80",
         "data": {
@@ -72,109 +41,59 @@ def update_object_2(object_id_2):
     }
     headers = {'Content-Type': 'application/json'}
     response = requests.put(
-        f'https://api.restful-api.dev/objects/{object_id_2}',
+        f'https://api.restful-api.dev/objects/{object_id}',
         json=body,
         headers=headers
     )
     print(response.json())
-    # assert response.status_code == 200, 'Status code is incorrect'
+    assert response.status_code == 200, 'Status code is incorrect'
     assert response.json()['name'] == 'Honor 80'
-    update_object_id_2 = response.json()['id']
-    return update_object_id_2
-
-
-# Очистка обьекта №2 после изменения
-def clear_object_2(update_object_id_2):
-    delete_response = requests.delete(f'https://api.restful-api.dev/objects/{update_object_id_2}')
-    print(delete_response.json())
-
-
-clear_object_2(update_object_2(create_object_2()))
+    update_object_put_id = response.json()['id']
+    return update_object_put_id
 
 
 # ----------------------------------------------------------------------------------------------------
 
 
-# Создание обьекта №3
-def create_object_3():
-    body = {
-        "name": "Samsung",
-        "data": {
-            "year": 3332,
-            "price": 331134.99,
-            "CPU model": "Intel Core i7",
-            "Hard disk size": "1 TB"
-        }
-    }
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(
-        'https://api.restful-api.dev/objects',
-        json=body,
-        headers=headers
-    )
-    print(response.json())
-    object_id_3 = response.json()['id']
-    return object_id_3
-
-
-# Изменение обьекта №3 методом PATCH
-def update_object_3(object_id_3):
+# Изменение обьекта методом PATCH
+def update_object_patch(object_id):
     body = {
         "name": "Iphone"
     }
     headers = {'Content-Type': 'application/json'}
     response = requests.patch(
-        f'https://api.restful-api.dev/objects/{object_id_3}',
+        f'https://api.restful-api.dev/objects/{object_id}',
         json=body,
         headers=headers
     )
     print(response.json())
     assert response.status_code == 200, 'Status code is incorrect'
     assert response.json()['name'] == 'Iphone'
-    update_object_id_3 = response.json()['id']
-    return update_object_id_3
-
-
-# Очистка обьекта №3 после изменения
-def clear_object_3(update_object_id_3):
-    delete_response = requests.delete(f'https://api.restful-api.dev/objects/{update_object_id_3}')
-    print(delete_response.json())
-
-
-clear_object_3(update_object_3(create_object_3()))
+    update_object_patch_id = response.json()['id']
+    return update_object_patch_id
 
 
 # ----------------------------------------------------------------------------------------------------
 
 
-# Создание обьекта №4 для удаления
-def create_object_4():
-    body = {
-        "name": "Oppo",
-        "data": {
-            "year": 3333,
-            "price": 4424.99,
-            "CPU model": "Intel Core i9",
-            "Hard disk size": "1 TB"
-        }
-    }
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(
-        'https://api.restful-api.dev/objects',
-        json=body,
-        headers=headers
-    )
-    print(response.json())
-    object_id_4 = response.json()['id']
-    return object_id_4
-
-
 # Удаление обьекта №4
-def clear_object_4(object_id_4):
-    delete_response = requests.delete(f'https://api.restful-api.dev/objects/{object_id_4}')
+def clear_object(object_id):
+    delete_response = requests.delete(f'https://api.restful-api.dev/objects/{object_id}')
     assert delete_response.status_code == 200, 'Status code is incorrect'
-    assert delete_response.json()['message'] == f'Object with id = {object_id_4} has been deleted.'
+    assert delete_response.json()['message'] == f'Object with id = {object_id} has been deleted.'
     print(delete_response.json())
 
 
-clear_object_4(create_object_4())
+# Основная логика
+
+# Создание объекта
+created_object_id = create_object()
+
+# Изменение объекта с помощью PUT
+updated_object_put_id = update_object_put(created_object_id)
+
+# Изменение объекта с помощью PATCH
+updated_object_patch_id = update_object_patch(updated_object_put_id)
+
+# Удаление объекта
+clear_object(updated_object_patch_id)
